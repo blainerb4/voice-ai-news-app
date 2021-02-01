18a5136bd62c1840075fb9eaf2bb85ed26275736
Matthew Harrison <matthewharrison@Matthews-MBP-2.attlocal.net>
2021-02-01 03:37:45 -0500

    added alan ai, material ui styles and news api. app is now voice responsive and brings up latest news.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
 import './App.css';
+import NewsCards from './components/news-cards/news-cards'
 
+const alanKey = '651d745ff81c7c8e76f19ff45490e1c52e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if(command === 'newHeadlines') {
+          setNewsArticles(articles);
+        }
+      }
+    })
+  }, [])
+
+  return (
+    <div>
+      <NewsCards articles={newsArticles} />
+    </div>
+  )
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +47,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
